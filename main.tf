8a5277ee8eb674f562c7bd8d527afe84666774b5
vikesh kumar <73430534+vikeshkumar8210@users.noreply.github.com>
2023-03-16 18:05:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,16 +5,23 @@ provider "aws" {
 #Create security group with firewall rules
 resource "aws_security_group" "my_security_group" {
   name        = var.security_group
-  description = "security group for Ec2 instance"
+  description = "launch-wizard-3 created 2023-03-16T09:39:24.780Z"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -23,10 +30,10 @@ resource "aws_security_group" "my_security_group" {
 
  # outbound from jenkis server
   egress {
-    from_port   = 0
-    to_port     = 65535
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["157.34.89.173/32"]
   }
 
   tags= {
@@ -35,7 +42,7 @@ resource "aws_security_group" "my_security_group" {
 }
 
 # Create AWS ec2 instance
-resource "aws_instance" "myFirstInstance" {
+resource "aws_instance" "terraformpipeline" {
   ami           = var.ami_id
   key_name = var.key_name
   instance_type = var.instance_type
@@ -46,9 +53,9 @@ resource "aws_instance" "myFirstInstance" {
 }
 
 # Create Elastic IP address
-resource "aws_eip" "myFirstInstance" {
+resource "aws_eip" "TerraformPipeline" {
   vpc      = true
-  instance = aws_instance.myFirstInstance.id
+  instance = aws_instance.TerraformPipeline.id
 tags= {
     Name = "my_elastic_ip"
   }
